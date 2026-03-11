c75d06ca60bfd070bc8dcf771374df835b61b0d3
Sihoon Kim <cveloper918@gmail.com>
2026-03-11 10:50:30 +0900

    Bullet.cpp 코드 스타일 개선 및 불필요 조건 제거
    코드 들여쓰기를 탭 기반으로 변경하여 가독성을 향상시켰습니다.
    OnDestroy에서 collider_ null 체크를 제거하고 항상 DeregisterCollider와 SAFE_DELETE를 호출하도록 수정했습니다.
    OnCollisionEnter에서 Destroy() 호출 위치를 조정하여 충돌 시 총알이 항상 제거되도록 명확하게 처리했습니다.
    전체적으로 불필요한 조건문을 제거하여 코드가 간결해졌습니다.

--- PlayGround/Project/Gameplay/Actors/Projectile/Bullet.cpp
+++ PlayGround/Project/Gameplay/Actors/Projectile/Bullet.cpp
@@ -4,52 +4,52 @@
 #include "Components/SphereCollider.h"
 
 Bullet::Bullet(GameObjectBase* _owner, _float _damage, _float _speed)
-    : owner_(_owner)
-    , damage_(_damage)
-    , speed_(_speed)
+	: owner_(_owner)
+	, damage_(_damage)
+	, speed_(_speed)
 {
 }
 
 _bool Bullet::Initialize()
 {
-    if (!__super::Initialize())
-        return false;
+	if (!__super::Initialize())
+		return false;
 
-    // 콜라이더 생성 및 설정
-    collider_ = new SphereCollider(5.f);
-    collider_->Visible(true);
-    RegisterComponent(collider_);
+	// 콜라이더 생성 및 설정
+	collider_ = new SphereCollider(5.f);
+	collider_->Visible(true);
+	RegisterComponent(collider_);
 
-    // 충돌 레이어 등록 (적의 총알은 플레이어와 충돌)
-    _ColMgr.RegisterCollider(CollisionLayer::EnemyBullet, collider_);
+	// 충돌 레이어 등록 (적의 총알은 플레이어와 충돌)
+	_ColMgr.RegisterCollider(CollisionLayer::EnemyBullet, collider_);
 
-    Finalize();
-    return true;
+	Finalize();
+	return true;
 }
 
 _int Bullet::Update(_double _delta_time)
 {
-    // 생존 시간 체크
-    elapsed_time_ += _delta_time;
-    if (elapsed_time_ >= lifetime_)
-    {
-        Destroy();
-        return UPDATE_CONTINUE;
-    }
+	// 생존 시간 체크
+	elapsed_time_ += _delta_time;
+	if (elapsed_time_ >= lifetime_)
+	{
+		Destroy();
+		return UPDATE_CONTINUE;
+	}
 
-    // 이동 (현재 Look 방향으로 이동)
-    const auto forward = transform_->Forward2D();
-    const auto current_pos = transform_->Position();
-    transform_->Position(current_pos + forward * speed_ * _delta_time);
+	// 이동 (현재 Look 방향으로 이동)
+	const auto forward = transform_->Forward2D();
+	const auto current_pos = transform_->Position();
+	transform_->Position(current_pos + forward * speed_ * _delta_time);
 
-    return UPDATE_CONTINUE;
+	return UPDATE_CONTINUE;
 }
 
 void Bullet::Render(_double _delta_time)
 {
-    // 총알 렌더링 (간단한 원으로 표현)
-    const auto pos = transform_->Position();
-    _DrawFunc::FillCircle(pos, collider_->Radius(), Colors::DeepGray);
+	// 총알 렌더링 (간단한 원으로 표현)
+	const auto pos = transform_->Position();
+	_DrawFunc::FillCircle(pos, collider_->Radius(), Colors::DeepGray);
 }
 
 void Bullet::DebugRender(_double _delta_time)
@@ -68,11 +68,8 @@ void Bullet::DebugRender(_double _delta_time)
 
 void Bullet::OnDestroy()
 {
-    if (collider_)
-    {
-		_ColMgr.DeregisterCollider(CollisionLayer::EnemyBullet, collider_);
-        SAFE_DELETE(collider_);
-    }
+	_ColMgr.DeregisterCollider(CollisionLayer::EnemyBullet, collider_);
+	SAFE_DELETE(collider_);
 }
 
 void Bullet::OnCollisionEnter(Collider* _this, Collider* _other)
@@ -81,6 +78,5 @@ void Bullet::OnCollisionEnter(Collider* _this, Collider* _other)
 		s_cast(IDamagable*, _handler)->GetDamage(damage_);
 		});
 
-        Destroy();  // 충돌 후 총알 제거
-    }
+	Destroy();  // 충돌 후 총알 제거
 }
