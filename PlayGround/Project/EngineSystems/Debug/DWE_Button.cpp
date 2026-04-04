e84078caaf820e76c79f3c1e24de731e97bb9cd4
Sihoon Kim <cveloper918@gmail.com>
2026-04-04 21:39:24 +0900

    디버그 윈도우에 버튼 및 체크박스 UI 요소 추가
    DWE_Button 및 DWE_CheckBox 클래스를 새로 추가하여 디버그 윈도우에서 버튼과 체크박스 UI 요소를 사용할 수 있도록 구현했습니다. 각 요소는 마우스 입력에 따라 상태를 갱신하고, 버튼 클릭 시 콜백 호출, 체크박스 클릭 시 값 토글 기능을 제공합니다. UI 요소의 크기 측정 및 렌더링 로직도 포함되어 있습니다.

---
+++ PlayGround/Project/EngineSystems/Debug/DWE_Button.cpp
@@ -0,0 +1,73 @@
+﻿#include "framework.h"
+#include "DWE_Button.h"
+
+_bool DWE_Button::IsPointInRect(const _Vector2& _point, const _RectF& _rect) const
+{
+	return (_point.x >= _rect.left && _point.x < _rect.right &&
+		_point.y >= _rect.top && _point.y < _rect.bottom);
+}
+
+_int DWE_Button::Update(_double _delta_time)
+{
+	UNREFERENCED_PARAMETER(_delta_time);
+
+	const _Vector2 mouse_pos = _InputMgr.MousePoint();
+	const _bool mouse_pressed = _InputMgr.Down(VK_LBUTTON);
+	const _bool mouse_down = _InputMgr.Pressed(VK_LBUTTON);
+	const _bool mouse_released = _InputMgr.Up(VK_LBUTTON);
+
+	is_hovered_ = IsPointInRect(mouse_pos, rect_);
+
+	if (mouse_pressed && is_hovered_)
+		is_pressed_ = true;
+
+	if (is_pressed_ && mouse_released)
+	{
+		if (is_hovered_ && on_click_)
+			on_click_();
+
+		is_pressed_ = false;
+	}
+
+	if (is_pressed_ && mouse_down == false && mouse_released == false)
+		is_pressed_ = false;
+
+	return UPDATE_CONTINUE;
+}
+
+_Vector2 DWE_Button::Measure() const
+{
+	const _Vector2 text_size = _DrawFunc::MeasureString(
+		label_,
+		font_size_,
+		Gdiplus::FontStyleRegular);
+
+	return _Vector2(
+		text_size.x + horizontal_padding_ * 2.f,
+		text_size.y + vertical_padding_ * 2.f);
+}
+
+void DWE_Button::Render(_double _delta_time)
+{
+	UNREFERENCED_PARAMETER(_delta_time);
+
+	_Color fill_color = background_color_;
+
+	if (is_pressed_)
+		fill_color = pressed_color_;
+	else if (is_hovered_)
+		fill_color = hover_color_;
+
+	_DrawFunc::FillRectangle(rect_, fill_color);
+	_DrawFunc::DrawRectangle(rect_, border_color_, 1.f);
+
+	_DrawFunc::DrawString(
+		rect_,
+		label_,
+		text_color_,
+		font_size_,
+		Gdiplus::FontStyleRegular,
+		Gdiplus::StringAlignmentCenter,
+		Gdiplus::StringAlignmentCenter,
+		true);
+}
---
+++ PlayGround/Project/EngineSystems/Debug/DWE_CheckBox.cpp
@@ -0,0 +1,106 @@
+﻿#include "framework.h"
+#include "DWE_CheckBox.h"
+
+_bool DWE_CheckBox::IsPointInRect(const _Vector2& _point, const _RectF& _rect) const
+{
+	return (_point.x >= _rect.left && _point.x < _rect.right &&
+		_point.y >= _rect.top && _point.y < _rect.bottom);
+}
+
+_RectF DWE_CheckBox::GetBoxRect() const
+{
+	const _float box_top = rect_.top + (rect_.Height() - box_size_) * 0.5f;
+
+	return _RectF(
+		rect_.left,
+		box_top,
+		rect_.left + box_size_,
+		box_top + box_size_);
+}
+
+_RectF DWE_CheckBox::GetTextRect() const
+{
+	return _RectF(
+		rect_.left + box_size_ + box_text_spacing_,
+		rect_.top,
+		rect_.right,
+		rect_.bottom);
+}
+
+_int DWE_CheckBox::Update(_double _delta_time)
+{
+	UNREFERENCED_PARAMETER(_delta_time);
+
+	if (value_ptr_ == nullptr)
+		return UPDATE_CONTINUE;
+
+	const _Vector2 mouse_pos = _InputMgr.MousePoint();
+	const _bool mouse_pressed = _InputMgr.Down(VK_LBUTTON);
+	const _bool mouse_down = _InputMgr.Pressed(VK_LBUTTON);
+	const _bool mouse_released = _InputMgr.Up(VK_LBUTTON);
+
+	is_hovered_ = IsPointInRect(mouse_pos, rect_);
+
+	if (mouse_pressed && is_hovered_)
+		is_pressed_ = true;
+
+	if (is_pressed_ && mouse_released)
+	{
+		if (is_hovered_)
+			*value_ptr_ = !(*value_ptr_);
+
+		is_pressed_ = false;
+	}
+
+	if (is_pressed_ && mouse_down == false && mouse_released == false)
+		is_pressed_ = false;
+
+	return UPDATE_CONTINUE;
+}
+
+_Vector2 DWE_CheckBox::Measure() const
+{
+	const _Vector2 text_size = _DrawFunc::MeasureString(
+		label_,
+		font_size_,
+		Gdiplus::FontStyleRegular);
+
+	const _float width = box_size_ + box_text_spacing_ + text_size.x;
+	const _float height = std::max(box_size_, text_size.y);
+
+	return _Vector2(width, height);
+}
+
+void DWE_CheckBox::Render(_double _delta_time)
+{
+	UNREFERENCED_PARAMETER(_delta_time);
+
+	const _RectF box_rect = GetBoxRect();
+	const _RectF text_rect = GetTextRect();
+
+	const _Color fill_color = is_hovered_ ? hover_color_ : background_color_;
+
+	_DrawFunc::FillRectangle(box_rect, fill_color);
+	_DrawFunc::DrawRectangle(box_rect, border_color_, 1.f);
+
+	if (value_ptr_ != nullptr && *value_ptr_)
+	{
+		const _RectF inner_rect(
+			box_rect.left + 3.f,
+			box_rect.top + 3.f,
+			box_rect.right - 3.f,
+			box_rect.bottom - 3.f);
+
+		_DrawFunc::FillRectangle(inner_rect, check_color_);
+	}
+
+	_DrawFunc::DrawString(
+		text_rect,
+		label_,
+		text_color_,
+		font_size_,
+		Gdiplus::FontStyleRegular,
+		Gdiplus::StringAlignmentNear,
+		Gdiplus::StringAlignmentCenter,
+		true);
+}
