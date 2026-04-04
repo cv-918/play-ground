ac4ca2f6357d80b91fd15f66136837e2f2baa165
Sihoon Kim <cveloper918@gmail.com>
2026-04-04 21:39:24 +0900

    디버그 윈도우에 버튼 및 체크박스 UI 요소 추가
    DWE_Button 및 DWE_CheckBox 클래스를 새로 추가하여 디버그 윈도우에서 버튼과 체크박스 UI 요소를 사용할 수 있도록 구현했습니다. 각 요소는 마우스 입력에 따라 상태를 갱신하고, 버튼 클릭 시 콜백 호출, 체크박스 클릭 시 값 토글 기능을 제공합니다. UI 요소의 크기 측정 및 렌더링 로직도 포함되어 있습니다.

---
+++ PlayGround/Project/EngineSystems/Debug/DWE_Button.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "DebugWindowElement.h"
+
+class DWE_Button final : public DebugWindowElement
+{
+public:
+	DWE_Button(const std::wstring& _label, std::function<void()> _on_click)
+		: DebugWindowElement(DebugWindowElementType::Button)
+		, label_(_label)
+		, on_click_(std::move(_on_click))
+	{
+	}
+
+	~DWE_Button() override DEFAULT;
+
+public:
+	_int Update(_double _delta_time) override;
+	_Vector2 Measure() const override;
+	void Render(_double _delta_time) override;
+
+private:
+	_bool IsPointInRect(const _Vector2& _point, const _RectF& _rect) const;
+
+private:
+	std::wstring label_;
+	std::function<void()> on_click_;
+
+	_float horizontal_padding_ = 10.f;
+	_float vertical_padding_ = 6.f;
+	_float font_size_ = 12.f;
+
+	_bool is_hovered_ = false;
+	_bool is_pressed_ = false;
+
+	_Color border_color_ = _Color(255, 40, 40, 40);
+	_Color background_color_ = _Color(255, 240, 240, 240);
+	_Color hover_color_ = _Color(255, 225, 225, 225);
+	_Color pressed_color_ = _Color(255, 200, 200, 200);
+	_Color text_color_ = Palette::Black;
+};
---
+++ PlayGround/Project/EngineSystems/Debug/DWE_CheckBox.h
@@ -0,0 +1,43 @@
+﻿#pragma once
+
+#include "DebugWindowElement.h"
+
+class DWE_CheckBox final : public DebugWindowElement
+{
+public:
+	DWE_CheckBox(const std::wstring& _label, _bool* _value_ptr)
+		: DebugWindowElement(DebugWindowElementType::CheckBox)
+		, label_(_label)
+		, value_ptr_(_value_ptr)
+	{
+	}
+
+	~DWE_CheckBox() override DEFAULT;
+
+public:
+	_int Update(_double _delta_time) override;
+	_Vector2 Measure() const override;
+	void Render(_double _delta_time) override;
+
+private:
+	_RectF GetBoxRect() const;
+	_RectF GetTextRect() const;
+	_bool IsPointInRect(const _Vector2& _point, const _RectF& _rect) const;
+
+private:
+	std::wstring label_;
+	_bool* value_ptr_ = nullptr;
+
+	_float box_size_ = 14.f;
+	_float box_text_spacing_ = 6.f;
+	_float font_size_ = 12.f;
+
+	_bool is_hovered_ = false;
+	_bool is_pressed_ = false;
+
+	_Color border_color_ = _Color(255, 40, 40, 40);
+	_Color background_color_ = _Color(255, 245, 245, 245);
+	_Color hover_color_ = _Color(255, 230, 230, 230);
+	_Color check_color_ = _Color(255, 60, 140, 60);
+	_Color text_color_ = Palette::Black;
+};
